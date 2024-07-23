34431a35c1540dd583cb0c75d1f8e29e54583fe7
jgbenine <joaobenine@gmail.com>
2024-07-23 20:54:38 -0300

    feat:  create routes and get user

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,16 @@ import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod
 import { getTasksById } from "./routes/get-task-id";
 import { deleteTask } from "./routes/delete-task";
 import { updateTask } from "./routes/update-task";
+import { createUser } from "./routes/create-user";
+import { getUserByEmail } from "./routes/get-user";
+import cors from "@fastify/cors"
 
 const app = fastify();
 
+//Registrando cors, alterar para rotas do front-end em prod.
+app.register(cors, {
+  origin: "*",
+})
 
 //Zod config Fastify
 app.setValidatorCompiler(validatorCompiler);
@@ -21,6 +28,8 @@ app.register(createTask);
 app.register(getTasksById);
 app.register(deleteTask);
 app.register(updateTask);
+app.register(createUser);
+app.register(getUserByEmail);
 
 
 app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
