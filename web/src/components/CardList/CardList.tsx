7e11ccd0a57bb1af4031d412adb26d6b7e3d1ad8
jgbenine <joaobenine@gmail.com>
2024-08-01 12:12:15 -0300

    feat: implement drag and drop logical

--- web/src/components/CardList/CardList.tsx
+++ web/src/components/CardList/CardList.tsx
@@ -1,12 +1,10 @@
 'use client';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ButtonPrimary } from '../Buttons/ButtonPrimary';
-import { Check } from 'lucide-react';
+import { TaskType } from '@/app/api/_server/tasks/tasks-server';
+import { CardListTask } from './CardListTask/CardListTask';
+import { DragDropContext, Droppable } from '@hello-pangea/dnd';
 import './CardList.scss';
-import { TasksServer, TaskType } from '@/app/api/_server/tasks/tasks-server';
-import { ModalUpdate } from '../Modais/ModalUpdate/ModalUpdate';
-import useModal from '@/hooks/useModal';
-import { useTask } from '@/contexts/TaskContext';
 
 type PropsLink = {
   btnLabel: string,
@@ -21,56 +19,50 @@ type PropsCardList = {
   variants?: "primary" | "secondary"
 } & PropsLink;
 
+
 export function CardList({ variants = "primary", titleCard, descriptionCard, descriptionCard_2, tasks, btnLabel, handleClickBtnCard }: PropsCardList) {
-  const { isShowing, openModal, activeModal, closeModal } = useModal();
-  const { refreshTasks } = useTask();
 
-
-  async function handleDeletTask(taskId: string) {
-    if (!taskId) return;
-    await TasksServer.deleteTask(taskId);
-    refreshTasks();
+  function reoderList(list: TaskType[], startPosition: number, endPosition: number) {
+    const result = Array.from(list);
+    const [removed] = result.splice(startPosition, 1)
+    result.splice(endPosition, 0, removed);
+    return result;
   }
 
-  async function handleTaskComplete(taskId: string) {
-    if (!taskId) return;
-    await TasksServer.updateTaskCompleted(taskId);
-    refreshTasks();
+  function onDragEnd(result: any) {
+    if (!result.destination) return;
+    const items = reoderList(tasks, result.source.index, result.destination.index)
+    tasks = items;
   }
-
 
   return (
     <div className={`cardList ${variants === 'secondary' ? 'cardList--secondary' : ''}`}>
       <h3 className="cardList__title">{titleCard}</h3>
       <p className="cardList__description">{descriptionCard}</p>
-      <p className="cardList__description cardList__description--highlight">{descriptionCard_2}</p>
-      <ul className="cardList__items">
-        {tasks.map((item: TaskType) => (
-          <li className="cardList-item" key={item.id}>
-            <div className="cardList-item__task">
-              <button className="cardList-item__btnCheck" onClick={() => handleTaskComplete(item.id)}>
-                <Check />
-              </button>
-              <p className="cardList-item__titleTask">{item.title}</p>
+      {descriptionCard_2 && <p className="cardList__description cardList__description--highlight">{descriptionCard_2}</p>}
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="taksCard" type="list" direction="vertical">
+          {(provided) => (
+            <div className="cardList__items"
+              ref={provided.innerRef}
+              {...provided.droppableProps}>
+              {tasks?.map((item: TaskType, index) => (
+                <CardListTask
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  indexDragDrop={index}
+                />
+              ))}
+              {provided.placeholder}
+              <ButtonPrimary
+                label={btnLabel}
+                onClick={handleClickBtnCard}
+              />
             </div>
-            <div className="cardList-actions">
-              {variants === 'primary' && <button className="cardList-actions__btn cardList-actions__btn--edit" onClick={() => openModal(`updateTaskHome${item.id}`)}>Edit</button>}
-              <button className="cardList-actions__btn cardList-actions__btn--delete" onClick={() => handleDeletTask(item.id)}>Delete</button>
-            </div>
-            {activeModal === `updateTaskHome${item.id}` &&
-              <ModalUpdate idTask={item.id}
-                isShowing={isShowing}
-                descriptionPlaceholder={item.description}
-                titlePlaceholder={item.title}
-                closeModal={closeModal} />}
-          </li>
-        ))}
-        <ButtonPrimary
-          label={btnLabel}
-          onClick={handleClickBtnCard}
-        />
-      </ul>
-
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   )
 }
